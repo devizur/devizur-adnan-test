a2778c14ef7950ad77d94561cf8e9de4e0a769d9
adnanalemran <adnanalemranontor@gmail.com>
2026-02-19 12:09:57 +0600

    refactor: remove unused interceptors and clean up bookingEngineUrlHttp configuration

--- src/lib/api/bookingEngineUrlHttp.ts
+++ src/lib/api/bookingEngineUrlHttp.ts
@@ -1,55 +1,10 @@
-import axios, { type AxiosError } from "axios";
+import axios from "axios";
 import { env } from "@/config";
-import { store } from "@/store/store";
-import { clearAuth } from "@/store/authSlice";
 
 const { BookingEngineUrl } = env();
 
 const bookingEngineUrlHttp = axios.create({
   baseURL: BookingEngineUrl,
 });
-
- 
-bookingEngineUrlHttp.interceptors.request.use(
-  (config) => {
-    const state = store.getState();
-    const tokenFromStore = state.auth?.token ?? null;
-
-    const tokenFromStorage =
-      typeof window !== "undefined"
-        ? localStorage.getItem("authToken")
-        : null;
-
-    const token = tokenFromStore ?? tokenFromStorage;
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    } else {
-      delete config.headers.Authorization;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
- 
-bookingEngineUrlHttp.interceptors.response.use(
-  (response) => response,
-  (error: AxiosError) => {
-    if (error.response?.status === 401) {
-     
-      if (typeof window !== "undefined") {
-        localStorage.removeItem("authToken");
-        localStorage.removeItem("user");
-        window.location.href = "/sign-in";
-      }
-
-      store.dispatch(clearAuth());
-    }
-
-    return Promise.reject(error);
-  }
-);
 
 export default bookingEngineUrlHttp;

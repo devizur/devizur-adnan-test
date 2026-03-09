4217abd57b5476fc02942fa0fa7d96fbf3b887ab
Adnan Al-Emran <adnanalemranontor@gmail.com>
2026-03-09 15:26:08 +0600

    Update FoodCard keys to include index for unique identification in FoodsPageSection and Step2FoodSelection components

--- src/components/homepage/FoodsPageSection.tsx
+++ src/components/homepage/FoodsPageSection.tsx
@@ -70,8 +70,8 @@ const FoodsPageSection: React.FC<FoodsPageSectionProps> = ({ searchTerm }) => {
     return (
         <section className="container mx-auto pb-20">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {paginatedFoods.map((food) => (
-                    <FoodCard key={food.id} item={food} />
+                {paginatedFoods.map((food, index) => (
+                    <FoodCard key={`${food.id}-${index}`} item={food} />
                 ))}
             </div>
             <Pagination
